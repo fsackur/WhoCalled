c15016a0e37137fae69f07c2771794b9a05063b4
Freddie Sackur <github@dustyfox.uk>
2022-07-23 13:06:37 +0100

    Implement methods and propeties of FunctionInfo

--- Classes/FunctionCallInfo.ps1
+++ Classes/FunctionCallInfo.ps1
@@ -1,13 +1,48 @@
 class FunctionCallInfo
 {
+    <#
+        .SYNOPSIS
+        A pseudo-child of System.Management.Automation.FunctionInfo that's also a tree node.
+
+        We can't inherit because all the constructors of FunctionInfo are marked internal.
+    #>
+
+    # Hot path - we'll implement directly
     [string]$Name
     [string]$Source
     [psmoduleinfo]$Module
+
+    # This class is a tree node
     [FunctionCallInfo]$CalledBy
     [System.Collections.Generic.IList[FunctionCallInfo]]$Calls
+    [int]$Depth
 
+    # Inner object; we'll delegate calls to this
     hidden [Management.Automation.FunctionInfo]$Function
-    hidden [int]$Depth
+    hidden static [string[]]$_InheritedProperties = (
+        'CmdletBinding',
+        'CommandType',
+        'DefaultParameterSet',
+        'Definition',
+        'Description',
+        'HelpFile',
+        # 'Module',
+        'ModuleName',
+        # 'Name',
+        'Noun',
+        'Options',
+        'OutputType',
+        'Parameters',
+        'ParameterSets',
+        'RemotingCapability',
+        'ScriptBlock',
+        # 'Source',
+        'Verb',
+        'Version',
+        'Visibility',
+        'HelpUri'
+    )
+
 
     FunctionCallInfo ([Management.Automation.FunctionInfo]$Function)
     {
@@ -16,5 +51,28 @@ class FunctionCallInfo
         $this.Source = $Function.Source
         $this.Module = $Function.Module
         $this.Calls = [Collections.Generic.List[FunctionCallInfo]]::new()
+
+        [FunctionCallInfo]::_InheritedProperties | ForEach-Object {
+            Add-Member ScriptProperty -InputObject $this -Name $_ -Value (
+                [scriptblock]::Create("`$this.Function.`$_")
+            ) -SecondValue (
+                [scriptblock]::Create("`$this.Function.`$_ = `$args[0]")
+            )
+        }
+    }
+
+    [bool] Equals([object]$obj)
+    {
+        return $this.Function.Equals($obj)
+    }
+
+    [Management.Automation.ParameterMetadata] ResolveParameter([string]$name)
+    {
+        return $this.Function.ResolveParameter($name)
+    }
+
+    [string] ToString()
+    {
+        return $_.Function.ToString()
     }
 }
