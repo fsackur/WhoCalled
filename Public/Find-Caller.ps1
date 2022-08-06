1aa9de7b8ee868079e3e68abe225a29f5044cf46
Freddie Sackur <github@dustyfox.uk>
2022-08-06 20:37:10 +0100

    Find-Caller accepts private functions

--- Public/Find-Caller.ps1
+++ Public/Find-Caller.ps1
@@ -75,7 +75,55 @@ function Find-Caller
     {
         if ($PSCmdlet.ParameterSetName -eq 'ByName')
         {
-            $Command = Get-Command $Name -ErrorAction Stop
+            $Source, $_Name = $Name -split '\\', 2
+            if ($_Name) {$Name = $_Name} else {$Source = ''}
+
+            $Message = "Command '$Name' was not found."
+            $QualifyTip = "Try qualifying the command name, e.g. 'PowerShellGet\Find-Module'."
+            try
+            {
+                $_Command = Get-Command $Name -Module $Source -ErrorAction Stop
+
+                # Get-Command does not error on wildcards
+                if (-not $_Command)
+                {
+                    Write-Error -Exception ([Management.Automation.CommandNotFoundException]::new($Message)) -ErrorAction Stop
+                }
+            }
+            catch [Management.Automation.CommandNotFoundException]
+            {
+                $_Command = $Commands | Where-Object {
+                    $_.Name -eq $Name -and
+                    (-not $Source -or $_.Source -eq $Source)
+                }
+
+                if (-not $_Command)
+                {
+                    $SourceModules = if ($Source)
+                    {
+                        Get-Module $Source -ListAvailable -ErrorAction Stop | Import-Module -PassThru -ErrorAction Stop
+                    }
+                    else
+                    {
+                        $Modules
+                    }
+
+                    $_Command = $SourceModules |
+                        ForEach-Object {$Name | Resolve-Command -Module $_ -ResolveAlias -ErrorAction Ignore} |
+                        Write-Output |
+                        Select-Object -ExpandProperty Command |
+                        Where-Object {(-not $Source) -or $_.Source -eq $Source}
+                }
+            }
+
+            if ($_Command.Count -ne 1)
+            {
+                if ($_Command) {$Message = "Multiple commands found with name '$Name'."}
+                if (-not $Source) {$Message = $Message, $QualifyTip -join ' '}
+                Write-Error -Exception ([Management.Automation.CommandNotFoundException]::new($Message)) -ErrorAction Stop
+            }
+
+            $Command = $_Command
         }
 
         $Call = [CallInfo]$Command
